efa7c65314f80aeddb8b5a54df5827b789852f3f
Cássio REis <cassio_2000@hotmail.com>
2018-05-20 13:16:27 -0300

    Atualização do modelo

--- modeloBanco.sql
+++ modeloBanco.sql
@@ -1,10 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  cassio
- * Created: 20/05/2018
- */
 
+CREATE TABLE PARTICIPANTE (
+    PARTICIPANTECOD INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    NOME VARCHAR(255),
+    EMAIL VARCHAR(255),
+    SENHA VARCHAR(255),
+    PRIMARY KEY (PARTICIPANTECOD)
+
+);
+
+
+CREATE TABLE atividade (
+    atvcod INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY ,
+    PARTICIPANTECOD INTEGER NOT NULL ,
+    eventocod INTEGER NOT NULL ,
+    PRIMARY KEY (atvcod),
+    FOREIGN KEY (eventocod) REFERENCES Evento(eventocod),
+    FOREIGN KEY (PARTICIPANTECOD) REFERENCES PARTICIPANTE(PARTICIPANTECOD)
+    
+);
