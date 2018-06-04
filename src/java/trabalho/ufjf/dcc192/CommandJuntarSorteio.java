bf5b9722c8eee79ef00c43cde8a565bf683abb0d
cassio <cassio@cassio-Aspire-E5-573>
2018-06-04 20:31:09 -0300

    Se a data expirar realizar o sorteio ao clicar em juntar Sorteio

--- src/java/trabalho/ufjf/dcc192/CommandJuntarSorteio.java
+++ src/java/trabalho/ufjf/dcc192/CommandJuntarSorteio.java
@@ -1,8 +1,9 @@
-
 package trabalho.ufjf.dcc192;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,44 +12,61 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CommandJuntarSorteio implements Comando{
-     
+public class CommandJuntarSorteio implements Comando {
+
     public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int flagJuntar=0;
-        
+        int flagJuntar = 0;
+
         try {
             int eventoCod = Integer.parseInt(request.getParameter("eventocod"));
             int usuarioLogado = Integer.parseInt(request.getParameter("participantecod"));
-            AtividadeDAO.getInstance().juntarSorteio(eventoCod,usuarioLogado);
-            
-            
-            
+            AtividadeDAO.getInstance().juntarSorteio(eventoCod, usuarioLogado);
+
             Evento evento = EventoDAO.getInstance().listEvento(eventoCod);
             List<Participante> participantes = AtividadeDAO.getInstance().listParticipanteParticipantes(eventoCod);
-           
-          
-            for ( int i =0 ; i<participantes.size();i++){
+
+            for (int i = 0; i < participantes.size(); i++) {
                 evento.getParticipantes().add(participantes.get(i));
             }
-             for (int i = 0; i < participantes.size(); i++) {
-                   flagJuntar=0;
+            for (int i = 0; i < participantes.size(); i++) {
+                flagJuntar = 0;
                 if (evento.getParticipantes().get(i).getCodigoParticipante() == usuarioLogado) {
                     flagJuntar = 1;
 
                 }
             }
 
-            request.setAttribute("eventocod", evento);
-            request.setAttribute("participantecod", usuarioLogado);
-            request.setAttribute("flagjuntar", flagJuntar);
-            RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/evento-descricao.jsp");
-            despachante.forward (request, response);
-            
-            
+            Date dataAtual = new Date();
+            if (evento.getDataSorteio().getTime() < dataAtual.getTime()) {
+
+                int i = 0;
+                int participanteCod = Integer.parseInt(request.getParameter("participantecod"));
+                Collections.shuffle(participantes);
+
+                for (i = 0; i < participantes.size() - 1; i++) {
+
+                    AtividadeDAO.getInstance().InsereSorteio(eventoCod, participantes.get(i).getCodigoParticipante(), participantes.get(i + 1).getCodigoParticipante());
+
+                }
+
+                AtividadeDAO.getInstance().InsereSorteio(eventoCod, participantes.get(i).getCodigoParticipante(), participantes.get(0).getCodigoParticipante());
+                EventoDAO.getInstance().atualizaEvento(eventoCod);
+                String redirect = "amigo-sorteado.html?participantecod=" + participanteCod + "&eventocod=" + eventoCod;
+                response.sendRedirect(redirect);
+
+            } else {
+
+                request.setAttribute("eventocod", evento);
+                request.setAttribute("participantecod", usuarioLogado);
+                request.setAttribute("flagjuntar", flagJuntar);
+                RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/evento-descricao.jsp");
+                despachante.forward(request, response);
+
+            }
         } catch (SQLException ex) {
             Logger.getLogger(CommandJuntarSorteio.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-     }
-    
+    }
+
 }
